bc1954cbdaad00f8ae4c28285fae388b2db5f3df
Lucas Dietrich <ld.adecy@gmail.com>
2023-09-17 17:09:45 +0200

    Add LPF and HPF filters

---
+++ src/utils/filters.c
@@ -0,0 +1,33 @@
+#include "filters.h"
+
+void lpf_init(struct lpf *lpf, float tau)
+{
+	lpf->tau = tau;
+	lpf->y	 = 0.0f;
+}
+
+float lpf_filter(struct lpf *lpf, float x, float dt)
+{
+	const float alpha = dt / (lpf->tau + dt);
+
+	lpf->y = alpha * x + (1.0f - alpha) * lpf->y;
+
+	return lpf->y;
+}
+
+void hpf_init(struct hpf *hpf, float tau)
+{
+	hpf->tau = tau;
+	hpf->y	 = 0.0f;
+	hpf->x	 = 0.0f;
+}
+
+float hpf_filter(struct hpf *hpf, float x, float dt)
+{
+	const float alpha = hpf->tau / (hpf->tau + dt);
+
+	hpf->y = alpha * hpf->y + alpha * (x - hpf->x);
+	hpf->x = x;
+
+	return hpf->y;
+}
---
+++ src/utils/filters.h
@@ -0,0 +1,23 @@
+#ifndef _FILTERS_H_
+#define _FILTERS_H_
+
+#include <stdint.h>
+
+struct lpf {
+	float tau;
+	float y;
+};
+
+struct hpf {
+	float tau;
+	float y;
+	float x;
+};
+
+void lpf_init(struct lpf *lpf, float tau);
+float lpf_filter(struct lpf *lpf, float x, float dt);
+
+void hpf_init(struct hpf *hpf, float tau);
+float hpf_filter(struct hpf *hpf, float x, float dt);
+
+#endif /* _FILTERS_H_ */
